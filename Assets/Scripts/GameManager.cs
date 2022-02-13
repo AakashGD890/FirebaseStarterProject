88fc21131761ff55ac1e443c2ba808db9bc549f3
AakashGD890 <aakashsolanki890@gmail.com>
2022-02-13 17:54:46 +0530

    Change Logs :-
    Create Game Manager in game scene so whenever game loads we will show a welcome message that includes our user name.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text messageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMessage();   
+    }
+
+    private void ShowMessage()
+    {
+        messageText.text = string.Format("Welcome, {0} In our game scene", References.userName);
+    }
+}
